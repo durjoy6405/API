24bbed1cea3dd4f4d7f5327627a381957f8f2e3f
Anam Mofazzal Durjoy <durjoy6405@gmail.com>
2024-05-21 00:37:11 +0600

    Api key issue is solved

--- Weather Api/js/app.js
+++ Weather Api/js/app.js
@@ -1,22 +1,28 @@
-const API_KEY = `d4edb3dea6570d501e74246ea689d301`;
+//const API_KEY = "d4edb3dea6570d501e74246ea689d301";
+//const city = "London";
 
-const loadTemperature = (city) => {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+function fetchWeather(city) {
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" +city+ "&appid=d4edb3dea6570d501e74246ea689d301";
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displayTemperature(data));
+    .then((data) => {
+        console.log(data);
+        displayTemperature(data);
+    });
 };
 
-const displayTemperature = (data) => {
-  // const temperature = document.getElementById("temperature");
-  console.log(data.name, data.main.temp);
-  // temperature.innerText = `${data.main.temp}`;
+fetchWeather(city);
 
-  setInnerText("temperature", data.main.temp);
-  setInnerText("weather", data.weather[0].main);
+function displayTemperature(data){
+    // const temperature = document.getElementById("temperature");
+    console.log(data.name, data.main.temp);
+    temperature.innerText = `${data.main.temp}`;
+
+    setInnerText("temperature", data.main.temp);
+    setInnerText("weather", data.weather[0].main);
 };
 
-const setInnerText = (id, text) => {
+function setInnerText (id, text){
   const element = document.getElementById(id);
   element.innerText = `${text}`;
   console.log(text);
@@ -31,7 +37,7 @@ document.getElementById("btn-search").addEventListener("click", () => {
   // const weather = data.weather[0].main;
   document.getElementById("weather").innerText = weather;
 
-  loadTemperature(city);
+  fetchWeather(city);
 });
 
-loadTemperature("dhaka");
+//loadTemperature("dhaka");
